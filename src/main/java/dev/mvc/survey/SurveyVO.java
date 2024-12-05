5b009eba54d7bea2c47c8780c6e8853c62689ea6
seong01394 <gogo3038@naver.com>
2024-12-05 16:45:36 +0900

    열공

--- src/main/java/dev/mvc/survey/SurveyVO.java
+++ src/main/java/dev/mvc/survey/SurveyVO.java
@@ -1,5 +1,26 @@
 package dev.mvc.survey;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter @Getter @ToString
 public class SurveyVO {
 
+  /** 설문조사 번호 */
+  private Integer surveyno; 
+  /** 설문조사 타이틀 */
+  private String survey_title;
+  /** 출력순서 */
+  private Integer seqno;
+  /** 질문 수 */
+  private Integer qa_num;
+  /** 질문내용 */
+  private String qa_contents;
+  /** 시작 날 */
+  private String start_date;
+  /** 완료 날 */
+  private String fin_date;
+  /** 작성일자 */
+  private String write_date = "";
 }
---
+++ src/main/webapp/WEB-INF/설문조사/survey.sql
@@ -0,0 +1,36 @@
+DROP TABLE survey;
+
+CREATE TABLE survey (
+    surveyno    NUMBER(5)   NOT NULL,  -- 설문조사 번호
+    survey_title    VARCHAR(50)   NOT NULL,  -- 설문조사 타이틀
+    seqno           NUMBER(10)      NOT NULL,  -- 출력 순서
+    qa_num          NUMBER(10)      NOT NULL, -- 질문수
+    qa_contents     VARCHAR(50)     NOT NULL, -- 질문 내용
+    start_date      VARCHAR(10)     NOT NULL, -- 시작날
+    fin_date        VARCHAR(10)     NOT NULL, -- 완료날
+    write_date      Date            NOT NULL, -- 최근 작성일자
+    selectno        NUMBER(10)      NOT NULL -- 선택 인원 수
+)
+    
+COMMENT ON TABLE survey is '설문조사';
+COMMENT ON COLUMN survey.surveyno is '설문조사 번호';
+COMMENT ON COLUMN survey.survey_title is '설문조사 타이틀';
+COMMENT ON COLUMN survey.seqno is '출력 순서';
+COMMENT ON COLUMN survey.qa_num is '질문수';
+COMMENT ON COLUMN survey.qa_contents is '질문내용'; 
+COMMENT ON COLUMN survey.start_date is '시작날';
+COMMENT ON COLUMN survey.fin_date is '완료날';
+COMMENT ON COLUMN survey.write_date is '최근 작성일자'; 
+COMMENT ON COLUMN survey.selectno is '선택 인원 수'; 
+
+DROP SEQUENCE SURVEY_SEQ;
+
+CREATE SEQUENCE SURVEY_SEQ
+START WITH 1         -- 시작 번호
+INCREMENT BY 1       -- 증가값
+MAXVALUE 9999999999  -- 최대값: 9999999999 --> NUMBER(10) 대응
+CACHE 2              -- 2번은 메모리에서만 계산
+NOCYCLE;             -- 다시 1부터 생성되는 것을 방지
+
+INSERT INTO survey(surveyno, survey_title, seqno, start_date, fin_date, qa_num, qa_contents, write_date)
+VALUES(survey_seq.nextval, '상위 3개의 팀 추천 설문조사', 1, '2024-12-01', '2024-12-02', 3, '한국인이 있는 팀이 좋습니까?', sysdate);
