f97425809125ed27b71ee0b569ce2d37ef27a9d2
seong01394 <gogo3038@naver.com>
2024-12-05 17:30:43 +0900

    열공

--- src/main/webapp/WEB-INF/설문조사/survey.sql
+++ src/main/webapp/WEB-INF/설문조사/survey.sql
@@ -8,7 +8,7 @@ CREATE TABLE survey (
     qa_contents     VARCHAR(50)     NOT NULL, -- 질문 내용
     start_date      VARCHAR(10)     NOT NULL, -- 시작날
     fin_date        VARCHAR(10)     NOT NULL, -- 완료날
-    write_date      Date            NOT NULL, -- 최근 작성일자
+    write_date      Date            NOT NULL, -- 작성일자
     selectno        NUMBER(10)      NOT NULL -- 선택 인원 수
 )
     
@@ -20,7 +20,7 @@ COMMENT ON COLUMN survey.qa_num is '질문수';
 COMMENT ON COLUMN survey.qa_contents is '질문내용'; 
 COMMENT ON COLUMN survey.start_date is '시작날';
 COMMENT ON COLUMN survey.fin_date is '완료날';
-COMMENT ON COLUMN survey.write_date is '최근 작성일자'; 
+COMMENT ON COLUMN survey.write_date is '작성일자'; 
 COMMENT ON COLUMN survey.selectno is '선택 인원 수'; 
 
 DROP SEQUENCE SURVEY_SEQ;
@@ -32,5 +32,8 @@ MAXVALUE 9999999999  -- 최대값: 9999999999 --> NUMBER(10) 대응
 CACHE 2              -- 2번은 메모리에서만 계산
 NOCYCLE;             -- 다시 1부터 생성되는 것을 방지
 
-INSERT INTO survey(surveyno, survey_title, seqno, start_date, fin_date, qa_num, qa_contents, write_date)
-VALUES(survey_seq.nextval, '상위 3개의 팀 추천 설문조사', 1, '2024-12-01', '2024-12-02', 3, '한국인이 있는 팀이 좋습니까?', sysdate);
+INSERT INTO survey(surveyno, survey_title, seqno, start_date, fin_date, qa_num, qa_contents, write_date, selectno)
+VALUES(survey_seq.nextval, '상위 3개의 팀 추천 설문조사', 1, '2024-12-01', '2024-12-30', 3, '한국인이 있는 팀이 좋습니까?', sysdate, 1);
+
+- 목록
+select surveyno, survey_title, seqno, start_date, fin_date, qa_num, qa_contents, write_date, selectno FROM survey ORDER BY surveyno ASC;
