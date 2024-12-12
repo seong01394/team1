f8a91fafc3254009f2e929062b6e6e88ffaa178e
KOHGH <gohgeonhee@gmail.com>
2024-12-12 18:16:09 +0900

    업데이트.

--- src/main/java/dev/mvc/club/ClubCont.java
+++ src/main/java/dev/mvc/club/ClubCont.java
@@ -15,6 +15,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import dev.mvc.cate.CateVO;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
@@ -24,6 +25,15 @@ public class ClubCont {
   @Autowired
   @Qualifier("dev.mvc.club.ClubProc")
   private ClubProcInter clubProc;
+  
+  /** 페이지당 출력할 레코드 갯수, nowPage는 1부터 시작 */
+  public int record_per_page = 10;
+
+  /** 블럭당 페이지 수, 하나의 블럭은 10개의 페이지로 구성됨 */
+  public int page_per_block = 510;
+
+  /** 페이징 목록 주소 */
+  private String list_file_name = "/club/list_search";
   
   public ClubCont() {
     System.out.println("-> ClubCont created."); 
@@ -60,7 +70,6 @@ public class ClubCont {
     return "/club/create"; // /templates/club/create.html
     } 
     
-    
    
     return "/club/msg"; // /templates/club/msg.html
   } 
@@ -74,6 +83,9 @@ public class ClubCont {
   @GetMapping(value = "/list_all")
   public String list_all(Model model) {
     ClubVO clubVO = new ClubVO();
+    
+    ArrayList<String> list_clubname = this.clubProc.clubnameset();
+    clubVO.setClubname(String.join("/", list_clubname));
 
     model.addAttribute("clubVO", clubVO);
 
@@ -92,11 +104,12 @@ public class ClubCont {
    */
   @GetMapping(value="/read/{clubno}")
   public String read(Model model, @PathVariable("clubno") Integer clubno,
-                          @RequestParam(name="word", defaultValue="") String word) {
+                          @RequestParam(name="word", defaultValue="") String word,
+                          @RequestParam(name="now_page", defaultValue="") int now_page) {
                    
     ClubVO clubVO = this.clubProc.read(clubno);
     model.addAttribute("clubVO", clubVO);
-    
+       
     ArrayList<ClubVOMenu> menu = this.clubProc.menu();
     model.addAttribute("menu", menu);
     
@@ -111,7 +124,8 @@ public class ClubCont {
   @GetMapping(value="/update/{clubno}")
   public String update(HttpSession session, Model model,
                              @PathVariable("clubno") Integer clubno,
-                             @RequestParam(name="word", defaultValue="") String word) {
+                             @RequestParam(name="word", defaultValue="") String word,
+                             @RequestParam(name="now_page", defaultValue="") int now_page) {
     
     return"/club/update";
   }
---
+++ src/main/webapp/WEB-INF/관리자/admin.sql
@@ -0,0 +1,35 @@
+/**********************************/
+/* Table Name: 관리자 */
+/**********************************/
+DROP TABLE admin;
+
+CREATE TABLE admin(
+    ADMINNO    NUMBER(10)    NOT NULL,
+    ID         VARCHAR(30)   NOT NULL UNIQUE, -- 아이디, 중복 안됨, 레코드를 구분 
+    PASSWD     VARCHAR(30)   NOT NULL, 
+    ADNAME      VARCHAR(20)   NOT NULL,
+    PRIMARY KEY (ADMINNO)
+);
+
+COMMENT ON TABLE admin is '관리자';
+COMMENT ON COLUMN admin.ADMINNO is '관리자 번호';
+COMMENT ON COLUMN admin.ID is '관리자 아이디';
+COMMENT ON COLUMN admin.PASSWD is '관리자 패스워드';
+COMMENT ON COLUMN admin.ADNAME is '관리자 이름';
+
+DROP SEQUENCE admin_SEQ;
+
+CREATE SEQUENCE admin_SEQ
+  START WITH 1              -- 시작 번호
+  INCREMENT BY 1          -- 증가값
+  MAXVALUE 9999999999 -- 최대값: 9999999999
+  CACHE 2                     -- 2번은 메모리에서만 계산
+  NOCYCLE;                   -- 다시 1부터 생성되는 것을 방지
+
+SELECT adminno, id, passwd, adname FROM admin ORDER BY adminno;
+
+INSERT INTO admin(adminno, id, passwd, adname)
+VALUES(admin_seq.nextval, 'emp1', '1234', '직원1');
+
+INSERT INTO admin(adminno, id, passwd, adname)
+VALUES(admin_seq.nextval, 'emp2', '1234', '직원2');
