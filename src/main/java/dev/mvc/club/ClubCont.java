d15cbb9baad2413d6c967b6e730d35b4576399e7
KOHGH <gohgeonhee@gmail.com>
2024-12-12 14:54:54 +0900

    업데이트.

--- src/main/java/dev/mvc/club/ClubCont.java
+++ src/main/java/dev/mvc/club/ClubCont.java
@@ -2,6 +2,7 @@ package dev.mvc.club;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -12,7 +13,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
 @Controller
@@ -81,7 +84,33 @@ public class ClubCont {
     model.addAttribute("menu", menu);
 
 
-    return "/cate/list_all"; // /templates/cate/list_all.html
+    return "/club/list_all"; // /templates/club/list_all.html
   }  
   
+  /**
+   * 조회
+   */
+  @GetMapping(value="/read/{clubno}")
+  public String read(Model model, @PathVariable("clubno") Integer clubno,
+                          @RequestParam(name="word", defaultValue="") String word) {
+                   
+    ClubVO clubVO = this.clubProc.read(clubno);
+    model.addAttribute("clubVO", clubVO);
+    
+    ArrayList<ClubVOMenu> menu = this.clubProc.menu();
+    model.addAttribute("menu", menu);
+    
+    model.addAttribute("word", word);
+    
+    return "/club/read";
+   }
+                      
+  
+  @GetMapping(value="/update/{clubno}")
+  public String update(HttpSession session, Model model,
+                             @PathVariable("clubno") Integer clubno,
+                             @RequestParam(name="word", defaultValue="") String word) {
+    
+    return"/club/update";
+  }
 }
